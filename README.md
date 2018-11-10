608e92a8adc3ad5185ac148f513021dcaf826c6e
CyberZHG <CyberZHG@gmail.com>
2018-11-10 16:23:04 +0800

    Initialize framework

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# keras-drop-block
+# Keras DropBlock
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-drop-block.svg)](https://travis-ci.org/CyberZHG/keras-drop-block)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-drop-block/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-drop-block)
+
+Implementation of [DropBlock](https://arxiv.org/pdf/1810.12890.pdf)
+
+## Install
+
+```bash
+pip install keras-drop-block
+```
+
+## Usage
+
+```python
+
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keras-drop-block',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/CyberZHG/keras-drop-block',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='DropBlock implemented in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
